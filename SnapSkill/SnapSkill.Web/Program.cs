0217947ffe872fd3d2d7e81ca808479fd8d13343
safi-abedin <kadersafi@2050>
2023-11-12 18:24:16 +0600

    Added Autofac ConFiguration

---
+++ SnapSkill/SnapSkill.Web/Program.cs
@@ -0,0 +1,53 @@
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using SnapSkill.Web;
+using SnapSkill.Web.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+//Added Autofac for Dependency injection
+builder.Host.UseServiceProviderFactory(x => new AutofacServiceProviderFactory());
+builder.Host.ConfigureContainer<ContainerBuilder>(x =>
+   x.RegisterModule(new WebModule())
+);
+
+// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapRazorPages();
+
+app.Run();
---
+++ SnapSkill/SnapSkill.Web/WebModule.cs
@@ -0,0 +1,13 @@
+﻿using Autofac;
+
+namespace SnapSkill.Web
+{
+    public class WebModule :Module
+    {
+
+        protected override void Load(ContainerBuilder builder)
+        {
+            
+        }
+    }
+}
